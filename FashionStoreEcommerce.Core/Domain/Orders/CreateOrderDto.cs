71ce77a4366d6a8973eb579e3df3edf4f9dedb5b
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 23:28:48 -0400

    Adicion de los Dtos de Order

---
+++ FashionStoreEcommerce.Core/Domain/Orders/CreateOrderDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class CreateOrderDto
+    {
+        public int CustomerId { get; set; }
+        public string Status { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public static Order ToEntity(CreateOrderDto dto)
+        {
+            return new Order
+            {
+                CustomerId = dto.CustomerId,
+                Status = dto.Status,
+                CreatedAt = dto.CreatedAt,
+            };
+        }
+    }
+}
---
+++ FashionStoreEcommerce.Core/Domain/Orders/OrderDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string Status { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public static Order ToEntity(OrderDto dto)
+        {
+            return new Order
+            {
+                Id = dto.Id,
+                CustomerId = dto.CustomerId,
+                Status = dto.Status,
+                CreatedAt = dto.CreatedAt
+
+            };
+        }
+        public static OrderDto FromEntity(Order entity)
+        {
+            return new OrderDto
+            {
+                Id = entity.Id,
+                CustomerId = entity.CustomerId,
+                Status = entity.Status,
+                CreatedAt = entity.CreatedAt,
+            };
+        }
+    }
+}
