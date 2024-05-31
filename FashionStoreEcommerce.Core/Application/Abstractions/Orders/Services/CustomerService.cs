bf854b3cd67f8190a7af77d9599c7771e9d53d76
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 22:44:47 -0400

    Adicion del servicio CustomerService

---
+++ FashionStoreEcommerce.Core/Application/Abstractions/Orders/Services/CustomerService.cs
@@ -0,0 +1,54 @@
+﻿using FashionStoreEcommerce.Core.Domain.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Application.Abstractions.Orders.Services
+{
+    public class CustomerService(
+        ICustomerRepository customerRepository,
+        IUnitOfWork uof
+        ) : ICustomerService
+    {
+        public async Task Add(Customer entity)
+        {
+            await customerRepository.Add( entity );
+            await uof.SaveChanges();
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var result = await customerRepository.Delete( id );
+            if (!result)
+            {
+                return false;
+            }
+            await uof.SaveChanges();
+            return true;
+
+        }
+
+        public async Task<List<Customer>> GetAll()
+        {
+            return await customerRepository.GetAll();
+        }
+
+        public async Task<Customer?> GetById(int id)
+        {
+            return await customerRepository.GetById( id );
+        }
+
+        public async Task<bool> Update(Customer entity)
+        {
+            var result = await customerRepository.Update( entity );
+            if (!result)
+            {
+                return false;
+            }
+            await uof.SaveChanges();
+            return true;
+        }
+    }
+}
