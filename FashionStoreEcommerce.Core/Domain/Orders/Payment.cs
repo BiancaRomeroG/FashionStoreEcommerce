c916be0bcef346e0132ade7c31f22fd7a0719305
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-31 00:58:00 -0400

    Adicion de la entidad Payment

---
+++ FashionStoreEcommerce.Core/Domain/Orders/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public decimal SubTotal { get; set; }
+        public decimal TaxAmount { get; set; }
+        public Order? Order { get; set; }
+    }
+}
