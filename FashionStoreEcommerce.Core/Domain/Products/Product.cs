ba2c311913e3c4c1e49d3420b4b64088f6951712
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 21:59:53 -0400

    Adicion de la entidad Product

---
+++ FashionStoreEcommerce.Core/Domain/Products/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public decimal Stock { get; set; }
+        public string Status { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public Category? Category { get; set; }
+    }
+}
