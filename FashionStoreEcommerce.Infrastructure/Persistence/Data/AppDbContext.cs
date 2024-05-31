2d7dcb14f7970fae3a01168dea4dcd79de4b5609
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 22:06:15 -0400

    Adicion de Product en AppDbContext

--- FashionStoreEcommerce.Infrastructure/Persistence/Data/AppDbContext.cs
+++ FashionStoreEcommerce.Infrastructure/Persistence/Data/AppDbContext.cs
@@ -6,5 +6,6 @@ namespace FashionStoreEcommerce.Infrastructure.Persistence.Data
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
