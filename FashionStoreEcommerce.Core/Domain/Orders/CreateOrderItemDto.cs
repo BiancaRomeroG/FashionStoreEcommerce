c4a0507e8d69d9f9bdb369eb83191a7d887cab20
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-31 00:08:11 -0400

    Adicion de los Dtos de OrderItem

---
+++ FashionStoreEcommerce.Core/Domain/Orders/CreateOrderItemDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class CreateOrderItemDto
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public static OrderItem ToEntity(CreateOrderItemDto dto)
+        {
+            return new OrderItem
+            {
+                OrderId = dto.OrderId,
+                ProductId = dto.ProductId,
+                Quantity = dto.Quantity,
+                UnitPrice = dto.UnitPrice
+
+            };
+        }
+    }
+}
---
+++ FashionStoreEcommerce.Core/Domain/Orders/OrderItemDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public static OrderItem ToEntity(OrderItemDto dto)
+        {
+            return new OrderItem
+            {
+                Id = dto.Id,
+                OrderId = dto.OrderId,
+                ProductId = dto.ProductId,
+                Quantity = dto.Quantity,
+                UnitPrice = dto.UnitPrice
+
+            };
+        }
+        public static OrderItemDto FromEntity(OrderItem entity)
+        {
+            return new OrderItemDto
+            {
+                Id = entity.Id,
+                OrderId = entity.OrderId,
+                ProductId = entity.ProductId,
+                Quantity = entity.Quantity,
+                UnitPrice = entity.UnitPrice,
+            };
+        }
+    }
+}
