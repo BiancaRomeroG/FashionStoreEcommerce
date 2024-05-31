c6b958db1ea9c7f3c560407fbec5ab5e41d87647
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 22:00:24 -0400

    Adicion de los Dtos de Product

---
+++ FashionStoreEcommerce.Core/Domain/Products/CreateProductDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Products
+{
+    public class CreateProductDto
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public decimal Stock { get; set; }
+        public string Status { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public static Product ToEntity(CreateProductDto dto)
+        {
+            return new Product
+            {
+                CategoryId = dto.CategoryId,
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price,
+                Stock = dto.Stock,
+                Status = dto.Status,
+                ImageUrl = dto.ImageUrl
+            };
+        }
+    }
+}
---
+++ FashionStoreEcommerce.Core/Domain/Products/ProductDto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Products
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public decimal Stock { get; set; }
+        public string Status { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+
+        public static Product ToEntity(ProductDto dto)
+        {
+            return new Product
+            {
+                Id = dto.Id,
+                CategoryId = dto.CategoryId,
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price,
+                Stock = dto.Stock,
+                Status = dto.Status,
+                ImageUrl = dto.ImageUrl
+            };
+        }
+
+        public static ProductDto FromEntity(Product entity)
+        {
+            return new ProductDto
+            {
+                Id = entity.Id,
+                CategoryId = entity.CategoryId,
+                Name = entity.Name,
+                Description = entity.Description,
+                Price = entity.Price,
+                Stock = entity.Stock,
+                Status = entity.Status,
+                ImageUrl = entity.ImageUrl,
+
+            };
+        }
+    }
+}
