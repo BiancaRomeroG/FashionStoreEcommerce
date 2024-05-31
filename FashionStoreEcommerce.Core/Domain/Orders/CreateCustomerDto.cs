c40c5c0424d83a1efd6ede8f8582f39a65e1d8b2
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 22:42:02 -0400

    Adicion de los Dtos Customer

---
+++ FashionStoreEcommerce.Core/Domain/Orders/CreateCustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using FashionStoreEcommerce.Core.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class CreateCustomerDto
+    {
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public int Nit { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public static Customer ToEntity(CreateCustomerDto dto)
+        {
+            return new Customer
+            {
+                Name = dto.Name,
+                Address = dto.Address,
+                Nit = dto.Nit,
+                CreatedAt = dto.CreatedAt
+            };
+        }
+    }
+}
---
+++ FashionStoreEcommerce.Core/Domain/Orders/CustomerDto.cs
@@ -0,0 +1,43 @@
+﻿using FashionStoreEcommerce.Core.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public int Nit { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public static Customer ToEntity(CustomerDto dto)
+        {
+            return new Customer
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Address = dto.Address,
+                Nit = dto.Nit,
+                CreatedAt = dto.CreatedAt
+
+            };
+        }
+
+        public static CustomerDto FromEntity(Customer entity)
+        {
+            return new CustomerDto
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Address = entity.Address,
+                Nit = entity.Nit,
+                CreatedAt = entity.CreatedAt
+            };
+        }
+    }
+}
