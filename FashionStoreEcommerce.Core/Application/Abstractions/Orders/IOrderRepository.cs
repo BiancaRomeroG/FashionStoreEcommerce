4cdf6b1f05ccfda8c59487746af698a994231841
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 23:29:50 -0400

    Adicion de la interface IOrderRepository

---
+++ FashionStoreEcommerce.Core/Application/Abstractions/Orders/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using FashionStoreEcommerce.Core.Domain.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Application.Abstractions.Orders
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+    }
+}
