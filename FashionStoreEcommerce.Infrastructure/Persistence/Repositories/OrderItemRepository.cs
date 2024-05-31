4d9c0807dcf094de667191cefdd974d09632c666
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-31 00:10:13 -0400

    Adicion de OrderItemRepository

---
+++ FashionStoreEcommerce.Infrastructure/Persistence/Repositories/OrderItemRepository.cs
@@ -0,0 +1,53 @@
+﻿using FashionStoreEcommerce.Core.Application.Abstractions.Orders;
+using FashionStoreEcommerce.Core.Domain.Orders;
+using FashionStoreEcommerce.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Infrastructure.Persistence.Repositories
+{
+    public class OrderItemRepository(AppDbContext context) : IOrderItemRepository
+    {
+        public async Task Add(OrderItem entity)
+        {
+            await context.OrderItems.AddAsync(entity);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var orderItem = await context.OrderItems.FindAsync(id);
+            if (orderItem == null)
+            {
+                return false;
+            }
+            context.OrderItems.Remove(orderItem);
+            return true;
+        }
+
+        public async Task<List<OrderItem>> GetAll()
+        {
+            return await context.OrderItems.ToListAsync();
+        }
+
+        public async Task<OrderItem?> GetById(int id)
+        {
+            return await context.OrderItems.FindAsync(id);
+
+        }
+
+        public async Task<bool> Update(OrderItem entity)
+        {
+            var orderItem = await context.OrderItems.FindAsync(entity.Id);
+            if (orderItem == null)
+            {
+                return false;
+            }
+            context.Entry(orderItem).CurrentValues.SetValues(entity);
+            return true;
+        }
+    }
+}
