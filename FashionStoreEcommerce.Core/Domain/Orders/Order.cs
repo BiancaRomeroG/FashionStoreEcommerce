7b1c297cb5b95810dbe78d031dbad5988fef7291
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 23:28:32 -0400

    Adicion de la entidad Order

---
+++ FashionStoreEcommerce.Core/Domain/Orders/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string Status { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public Customer? Customer { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; } = [];
+    }
+}
