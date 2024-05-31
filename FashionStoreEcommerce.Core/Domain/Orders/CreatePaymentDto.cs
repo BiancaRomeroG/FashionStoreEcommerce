1bc10f89a5486b9a025bae62f3693659221bd752
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-31 00:58:14 -0400

    Adicion de los Dtos de Payment

---
+++ FashionStoreEcommerce.Core/Domain/Orders/CreatePaymentDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class CreatePaymentDto
+    {
+        public int OrderId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public decimal SubTotal { get; set; }
+        public decimal TaxAmount { get; set; }
+        public static Payment ToEntity(CreatePaymentDto dto)
+        {
+            return new Payment
+            {
+                OrderId = dto.OrderId,
+                PaymentDate = dto.PaymentDate,
+                PaymentMethod = dto.PaymentMethod,
+                SubTotal = dto.SubTotal,
+                TaxAmount = dto.TaxAmount
+            };
+        }
+    }
+}
---
+++ FashionStoreEcommerce.Core/Domain/Orders/PaymentDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStoreEcommerce.Core.Domain.Orders
+{
+    public class PaymentDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public decimal SubTotal { get; set; }
+        public decimal TaxAmount { get; set; }
+        public static Payment ToEntity(PaymentDto dto)
+        {
+            return new Payment
+            {
+                Id = dto.Id,
+                OrderId = dto.OrderId,
+                PaymentDate = dto.PaymentDate,
+                PaymentMethod = dto.PaymentMethod,
+                SubTotal = dto.SubTotal,
+                TaxAmount = dto.TaxAmount
+            };
+        }
+        public static PaymentDto FromEntity(Payment entity)
+        {
+            return new PaymentDto
+            {
+                Id = entity.Id,
+                OrderId = entity.OrderId,
+                PaymentDate = entity.PaymentDate,
+                PaymentMethod = entity.PaymentMethod,
+                SubTotal = entity.SubTotal,
+                TaxAmount = entity.TaxAmount
+            };
+        }
+    }
+}
