9d82cf60daccd29dec7304091582c35c5261f7f7
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 23:31:31 -0400

    Adicion del controlador OrdersController

---
+++ FashionStoreEcommerce.API/Controllers/OrdersController.cs
@@ -0,0 +1,84 @@
+﻿using FashionStoreEcommerce.Core.Application.Abstractions.Orders;
+using FashionStoreEcommerce.Core.Application.Abstractions.Orders.Services;
+using FashionStoreEcommerce.Core.Application.Products.Services;
+using FashionStoreEcommerce.Core.Domain.Orders;
+using FashionStoreEcommerce.Core.Domain.Products;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FashionStoreEcommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController(IOrderService orderService) : ControllerBase
+    {
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrder()
+        {
+            var orders = await orderService.GetAll();
+            var orderDtos = orders
+                .Select(OrderDto.FromEntity);
+
+            return Ok(orderDtos);
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDto>> GetOrder(int id)
+        {
+            var order = await orderService.GetById(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var dto = OrderDto.FromEntity(order);
+            return Ok(dto);
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, OrderDto order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            var entity = OrderDto.ToEntity(order);
+            var result = await orderService.Update(entity);
+
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<ActionResult<OrderDto>> PostOrder(CreateOrderDto createOrder)
+        {
+            var order = CreateOrderDto.ToEntity(createOrder);
+            await orderService.Add(order);
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var result = await orderService.Delete(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
