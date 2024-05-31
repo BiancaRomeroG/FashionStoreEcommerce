e859cf6fe7cb3a819633b7de3fab001599007de7
BiancaRomeroG <biancaromeroguz@gmail.com>
2024-05-30 22:03:24 -0400

    Adicion del Servicio ProductService

---
+++ FashionStoreEcommerce.Core/Application/Products/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using FashionStoreEcommerce.Core.Application.Abstractions;
+using FashionStoreEcommerce.Core.Domain.Products;
+
+namespace FashionStoreEcommerce.Core.Application.Products.Services
+{
+    public class ProductService(
+        IProductRepository productRepository,
+        ICategoryRepository categoryRepository,
+        IUnitOfWork uof
+        ) : IProductService
+
+
+    {
+        public async Task Add(Product entity)
+        {
+            var category = await categoryRepository.GetById(entity.CategoryId);
+            if (category == null)
+            {
+                throw new Exception("Category not found");
+            }
+            await productRepository.Add(entity);
+            await uof.SaveChanges();
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var result = await productRepository.Delete(id);
+            if (!result)
+            {
+                return false;
+            }
+            await uof.SaveChanges();
+            return true;
+        }
+
+        public async Task<List<Product>> GetAll()
+        {
+            return await productRepository.GetAll();
+        }
+
+        public async Task<Product?> GetById(int id)
+        {
+            return await productRepository.GetById(id);
+        }
+
+        public async Task<bool> Update(Product entity)
+        {
+            var result = await productRepository.Update(entity);
+            if (!result)
+            {
+                return false;
+            }
+            await uof.SaveChanges();
+            return true;
+        }
+    }
+}
